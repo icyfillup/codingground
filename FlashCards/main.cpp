ae538c2490538526d1cbcad9943667966ed06abd
icyfillup <phil10tran@gmail.com>
2015-10-04 09:40:36 -0700

    added code for file input and output

--- FlashCards/main.cpp
+++ FlashCards/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
-	std::cout << "こんにちは世界" << std::endl;
-	system("PAUSE");
-    return 0;
+    std::cout << "こんにちは世界" << std::endl;
+
     
+    std::string line;
+    std::ifstream myfile("FlashCards.txt");
+    
+    if(myfile.is_open())
+    {
+        while(!myfile.eof())
+        {
+            getline(myfile, line);
+            std::cout << line << std::endl;
+        }
+        myfile.close();
+    }
+    else
+        std::cout << "Unable to open file" << std::endl;
+
+    
+    std::ofstream otherFile;
+    otherFile.open("FlashCards.txt");
+    otherFile.close();
+
+    
+    system("PAUSE");
+    return 0;    
 }
